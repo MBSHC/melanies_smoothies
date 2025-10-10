bfa10ef446afbaf7a7e9a0102157dd9c103e3604
MBSHC <168001673+MBSHC@users.noreply.github.com>
2025-10-10 14:49:05 +0800

    Update streamlit_app.py
    A Strange-Looking Statement That Will Get Us the "Search On" Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #convert the Snowpark Dataframe to a Pandans Datafrae so we can use teh LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 # multiselect
 ingredients_list = st.multiselect('Choose up to 5 ingredients:'
