538083f885e93ce733a1c480cefec6e738d9b202
MBSHC <168001673+MBSHC@users.noreply.github.com>
2025-10-10 13:16:32 +0800

    Update streamlit_app.py
    move the code of getting active session from line 19 to line 6 -  try troubleshooting with error asking me to craete a session first before calling funciton udf

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-
+session = get_active_session()
 # Write directly to the app
 # if you're new to Streamlit,** check out our easy-to-follow guides at
 # (https://docs.streamlit.io).
@@ -16,7 +16,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+# session = get_active_session() - try troubleshooting with error asking me to craete a session first before calling funciton udf
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
